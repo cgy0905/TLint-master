b655ec35b4369bf5a78fbec9de2d2c757dcf45c7
chenguoyu <cgy0905@163.com>
2019-04-23 20:01:50 +0800

    fix ContentActivity

--- hupu/src/main/java/com/cgy/hupu/module/content/ContentActivity.java
+++ hupu/src/main/java/com/cgy/hupu/module/content/ContentActivity.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.Display;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
