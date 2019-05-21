0cf964bc29286f9ef4f03a301e83ccd66d8fec84
chenguoyu <1020110023@qq.com>
2019-05-21 10:39:49 +0800

    完成收藏帖子逻辑

---
+++ hupu/src/main/java/com/cgy/hupu/module/thread/collect/CollectThreadListContract.java
@@ -0,0 +1,9 @@
+package com.cgy.hupu.module.thread.collect;
+
+/**
+ * @author cgy
+ * @desctiption
+ * @date 2019/5/21 10:03
+ */
+public interface CollectThreadListContract {
+}
---
+++ hupu/src/main/java/com/cgy/hupu/module/thread/collect/ThreadCollectPresenter.java
@@ -0,0 +1,9 @@
+package com.cgy.hupu.module.thread.collect;
+
+/**
+ * @author cgy
+ * @desctiption
+ * @date 2019/5/21 10:13
+ */
+public class ThreadCollectPresenter {
+}
